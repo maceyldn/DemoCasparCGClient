33ffd5e129756b1794c873ae2b563793c0fec5e8
Andy Mace <andy.mace@mediauk.net>
2012-11-01 20:56:42 +0000

    initial

---
+++ DemoCasparCGClient/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace DemoCasparCGClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolstriplabelAMCPConnected = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.AllowMerge = false;
+            this.statusStrip1.GripMargin = new System.Windows.Forms.Padding(0);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolstriplabelAMCPConnected});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 237);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(565, 25);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolstriplabelAMCPConnected
+            // 
+            this.toolstriplabelAMCPConnected.BackColor = System.Drawing.Color.Red;
+            this.toolstriplabelAMCPConnected.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.toolstriplabelAMCPConnected.Name = "toolstriplabelAMCPConnected";
+            this.toolstriplabelAMCPConnected.Size = new System.Drawing.Size(550, 20);
+            this.toolstriplabelAMCPConnected.Spring = true;
+            this.toolstriplabelAMCPConnected.Text = "AMCP Disconnected";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 262);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            this.Text = "Demo App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolstriplabelAMCPConnected;
+    }
+}
+
---
+++ DemoCasparCGClient/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Security.Principal;
+using System.IO;
+using Svt.Caspar;
+
+namespace DemoCasparCGClient
+{
+    public partial class Form1 : Form
+    {
+        //Main Caspar Device
+        private CasparDevice casparDevice = new CasparDevice();
+
+        //AMCP/OSC Connection Bits
+        public string caspar_server_IP { get; set; }
+        public int caspar_server_OSC_port { get; set; }
+        public int caspar_server_AMCP_port { get; set; }
+        private bool boolIsClientConnected = false;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            caspar_server_IP = ConfigurationManager.AppSettings["ServerIP"];
+            caspar_server_OSC_port = Int32.Parse(ConfigurationManager.AppSettings["OSCPort"]);
+            caspar_server_AMCP_port = Int32.Parse(ConfigurationManager.AppSettings["AMCPPort"]);
+           
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ChangeControlStatus(false);
+            SetupCasparCGDevice();
+        }
+
+        private void SetupCasparCGDevice()
+        {
+            casparDevice.Settings.Hostname = caspar_server_IP;
+            casparDevice.Settings.Port = caspar_server_AMCP_port;
+            casparDevice.Settings.AutoConnect = false;
+            casparDevice.Connected += new EventHandler<Svt.Network.NetworkEventArgs>(caspar_AMCP_Connected);
+            casparDevice.Disconnected += new EventHandler<Svt.Network.NetworkEventArgs>(caspar_AMCP_Disconnected);
+            casparDevice.FailedConnect += new EventHandler<Svt.Network.NetworkEventArgs>(casparDevice_AMCPFailed_Connect);
+            casparDevice.Connect();
+        }
+
+        void caspar_AMCP_Connected(object sender, Svt.Network.NetworkEventArgs e)
+        {
+            Console.WriteLine("Caspar AMCP Client Connected");
+            ChangeControlStatus(true);
+
+            this.Invoke(new MethodInvoker(delegate()
+            {
+                toolstriplabelAMCPConnected.BackColor = Color.Green;
+                toolstriplabelAMCPConnected.Text = "AMCP Connected";
+            }));
+        }
+
+        void caspar_AMCP_Disconnected(object sender, Svt.Network.NetworkEventArgs e)
+        {
+            Console.WriteLine("Caspar AMCP Client Disconnected");
+            casparDevice_AMCPFailed_Connect(sender, e);
+        }
+
+        void casparDevice_AMCPFailed_Connect(object sender, Svt.Network.NetworkEventArgs e)
+        {
+            
+            this.Invoke(new MethodInvoker(delegate()
+            {
+                toolstriplabelAMCPConnected.BackColor = Color.Red;
+                toolstriplabelAMCPConnected.Text = "AMCP Disconnected";
+            }));
+            //MessageBox.Show("Caspar Device has gone away");
+            ChangeControlStatus(false);
+
+            DialogResult result = MessageBox.Show("Cannot connect to CasparCG Server: " + caspar_server_IP + "\nDo you wish to wait to reconnect?", "Important Query", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result != DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+
+            casparDevice.Connect();
+
+        }
+
+        private void ChangeControlStatus(bool p)
+        {
+            this.Invoke(new MethodInvoker(delegate()
+            {
+                foreach (Control c in this.Controls)
+                {
+                    c.Enabled = p;
+                    //c.Visible = p;
+                }
+            }));
+        }
+
+               
+    }
+}
